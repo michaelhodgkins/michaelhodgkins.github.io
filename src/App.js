c158442aa0da718727b0e411ab0b0e9b59c060f2
Michael Hodgkins <mhodgkins1997@gmail.com>
2022-09-26 20:28:04 +0100

    added theme and changed position

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
 import { Route, Routes } from "react-router-dom";
 import LandingPage from './pages/LandingPage';
-
+import ResponsiveNav from "./components/Navbar/Navbar"
 
 function App() {
   return (
     <div className='App'>
+      <ResponsiveNav />
       <Routes>
         <Route path="/" element={<LandingPage />} />
       </Routes>
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -3,18 +3,37 @@ import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import Button from '@mui/material/Button';
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import "./Navbar.css";
 
-export default function ButtonAppBar() {
+const theme = createTheme({
+    palette: {
+        primary: {
+            main: "#A59AE6"
+        },
+        secondary: {
+            main: "#F4F4F5"
+        },
+        info: {
+            main: "#000000"
+        }
+    }
+
+
+
+})
+export default function ResponsiveNav() {
   return (
+    <ThemeProvider theme={theme}>
     <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static">
-        <Toolbar>
-          <Button>Projects</Button>
-          <Button> Contact Me</Button>
-          <Button> About Me</Button>
-          
+      <AppBar position="static" sx={{borderBottom: 1}}>
+        <Toolbar sx={{display: 'flex', justifyContent: 'flex-end', color: 'info'}}>
+          <Button color='info'>Projects</Button>
+          <Button color='info'> About Me</Button>
+          <Button color='info'> Contact Me</Button>
         </Toolbar>
       </AppBar>
     </Box>
+    </ThemeProvider>
   );
 }
