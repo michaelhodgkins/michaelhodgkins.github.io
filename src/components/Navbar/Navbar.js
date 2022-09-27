47e636370a8bbf0398889136d08a98b94f785c10
Michael Hodgkins <mhodgkins1997@gmail.com>
2022-09-27 16:30:54 +0100

    changed to floating nav bar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,7 +1,5 @@
 import * as React from 'react';
-import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
 import Button from '@mui/material/Button';
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 import { Link } from 'react-router-dom';
@@ -27,14 +25,10 @@ export default function ResponsiveNav() {
   return (
     <ThemeProvider theme={theme}>
     <Box>
-      <AppBar position="static" sx={{borderBottom: 1}}>
-        <Toolbar sx={{display: 'flex', justifyContent: 'flex-end', color: 'info'}}>
         <Button color='info'><Link style={{textDecoration: 'none', color: 'black'}} to="/"> Home</Link></Button>
           <Button color='info'><Link style={{textDecoration: 'none', color: 'black'}} to="/extra-projects"> Extra Projects</Link></Button>
           <Button color='info'><Link style={{textDecoration: 'none', color: 'black'}} to="/about"> About Me</Link></Button>
           <Button color='info'><Link style={{textDecoration: 'none', color: 'black'}} to="/contact-me"> Contact Me</Link></Button>
-        </Toolbar>
-      </AppBar>
     </Box>
     </ThemeProvider>
   );
